040234f509328c75f953b2c87b901511dcfe837b
Kaushik Biswas <kbutsho@gmail.com>
2026-04-03 14:00:06 +0600

    redirect to login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
