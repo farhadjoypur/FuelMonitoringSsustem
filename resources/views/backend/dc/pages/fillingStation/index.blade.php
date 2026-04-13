6312101fd2d8ea276662bc6fce753dc8aada654b
Kaushik Biswas <kbutsho@gmail.com>
2026-04-13 10:24:03 +0600

    added owner_name error msg

--- resources/views/backend/dc/pages/fillingStation/index.blade.php
+++ resources/views/backend/dc/pages/fillingStation/index.blade.php
@@ -412,8 +412,11 @@
 
                         <div class="mb-3">
                             <label class="modal-label">Owner Name</label>
-                            <input type="text" class="form-control" name="owner_name"
-                                value="{{ old('owner_name') }}">
+                            <input type="text" class="form-control @error('owner_name') is-invalid @enderror"
+                                name="owner_name" value="{{ old('owner_name') }}">
+                            @error('owner_name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
                         </div>
 
                         <div class="mb-3">
